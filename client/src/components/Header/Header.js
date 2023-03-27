716702921d0773cc80398aeb010ac2499910255e
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-03-28 00:36:25 +0400

    feat: created Code Challenge From Verifier

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -71,6 +71,37 @@ export const Header = () => {
       return ("0" + dec.toString(16)).substr(-2);
     }
 
+
+
+
+  const sha256 = (plain) => {
+        // returns promise ArrayBuffer
+        const encoder = new TextEncoder();
+        const data = encoder.encode(plain);
+        return window.crypto.subtle.digest("SHA-256", data);
+    }
+
+
+    const base64urlencode = (a) => {
+        var str = "";
+        var bytes = new Uint8Array(a);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            str += String.fromCharCode(bytes[i]);
+        }
+        return btoa(str)
+            .replace(/\+/g, "-")
+            .replace(/\//g, "_")
+            .replace(/=+$/, "");
+    }
+
+    async function generateCodeChallengeFromVerifier(v) {
+        var hashed = await sha256(v);
+        var base64encoded = base64urlencode(hashed);
+        return base64encoded;
+    }
+
+
     return (
     <div className='header'>
       <Link to={'/'}>Main page</Link>
