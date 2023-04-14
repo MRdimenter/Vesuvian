7a01138be35d0d75fcfe824e8965f1263ec9aaa1
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-04-15 01:30:37 +0400

    feat: added logging

--- server/security/src/main/java/ru/vesuvian/service/security/controller/UserController.java
+++ server/security/src/main/java/ru/vesuvian/service/security/controller/UserController.java
@@ -1,14 +1,26 @@
 package ru.vesuvian.service.security.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
+    
     @GetMapping("/test")
     public String getTestData() {
+        log.info("Sent response with test data");
         return "test data";
     }
 
