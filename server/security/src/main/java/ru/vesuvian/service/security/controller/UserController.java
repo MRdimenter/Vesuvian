91a71fbb4e52b177204d1295c92ac7177801a74e
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-04-16 01:34:15 +0400

    refactor: removed unnecessary imports

--- server/security/src/main/java/ru/vesuvian/service/security/controller/UserController.java
+++ server/security/src/main/java/ru/vesuvian/service/security/controller/UserController.java
@@ -1,17 +1,9 @@
 package ru.vesuvian.service.security.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/user")
@@ -23,5 +15,5 @@ public class UserController {
         log.info("Sent response with test data");
         return "test data";
     }
-
+    
 }
