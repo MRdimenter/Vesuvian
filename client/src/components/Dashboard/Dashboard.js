8a05d27bdb379057cf47f7547d14c8162d155600
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-05-05 02:09:54 +0400

    feat: added registration field

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,9 +2,45 @@ import './dashboard.css'
 import React from 'react';
 
 export default function Dashboard() {
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    
+
+    
+    //getTestDataFromResourceServer(access_token);
+  }
+
   return (
-    <div className='dashboard'>
-      <h2>Dashboard</h2>
+    <div className="regist-wrapper">
+      <h1>Регистрация</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="username" onChange={e => e} />
+        </label>
+        <label>
+          <p>FirstName</p>
+          <input type="firstname" onChange={e => e} />
+        </label>
+        <label>
+          <p>Lastname</p>
+          <input type="lastname" onChange={e => e} />
+        </label>
+        <label>
+          <p>Email</p>
+          <input type="email" onChange={e => e} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => e} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
