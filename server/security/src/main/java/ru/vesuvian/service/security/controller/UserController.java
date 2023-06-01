428aaf9c7f4afd8bb3b69ccd350cc631de6e8035
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-06-02 01:10:23 +0400

    feat: create method "createUser"

--- server/security/src/main/java/ru/vesuvian/service/security/controller/UserController.java
+++ server/security/src/main/java/ru/vesuvian/service/security/controller/UserController.java
@@ -1,19 +1,29 @@
 package ru.vesuvian.service.security.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.vesuvian.service.security.model.User;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("api/v1/users")
 @Slf4j
 public class UserController {
-    
+
+
     @GetMapping("/test")
     public String getTestData() {
         log.info("Sent response with test data");
         return "test data";
     }
-    
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody User user) {
+        log.info(user.getUsername());
+        log.info(user.getFirstName());
+        log.info(user.getLastName());
+        log.info(user.getEmail());
+
+    }
 }
