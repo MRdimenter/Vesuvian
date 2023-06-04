d332abc45bcaa81695ae4c260e280807e0311210
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-06-04 20:48:20 +0400

    refactor: renamed user to customer

--- server/security/src/main/java/ru/vesuvian/service/security/model/Customer.java
+++ server/security/src/main/java/ru/vesuvian/service/security/model/Customer.java
@@ -3,12 +3,11 @@ package ru.vesuvian.service.security.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class User {
+public class Customer {
     private String username;
     private String firstName;
     private String lastName;
