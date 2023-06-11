e32ff3d35bac786b3c62761d52ab8359e108363a
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-06-11 19:04:57 +0400

    feat: created method handleUserExistsException

--- server/security/src/main/java/ru/vesuvian/service/security/exception/ApiExceptionHandler.java
+++ server/security/src/main/java/ru/vesuvian/service/security/exception/ApiExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.ZonedDateTime;
@@ -21,6 +22,15 @@ public class ApiExceptionHandler {
         var apiException = new ApiException(e.getMessage(), status, ZonedDateTime.now());
 
         log.info("API Exception: Status - {}, Message - {}", status, e.getMessage());
+
+        return new ResponseEntity<>(apiException, status);
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(UserExistsException.class)
+    public ResponseEntity<Object> handleUserExistsException(UserExistsException e) {
+        var status = HttpStatus.CONFLICT;
+        var apiException = new ApiException(e.getMessage(), status, ZonedDateTime.now());
 
         return new ResponseEntity<>(apiException, status);
     }
