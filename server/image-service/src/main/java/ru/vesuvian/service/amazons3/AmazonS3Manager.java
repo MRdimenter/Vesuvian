e7f08a72faa518e1e5b95fff89b77042f91145fa
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-07-22 00:40:04 +0400

    feat: added a separate AmazonS3Manager class for working with object storage

---
+++ server/image-service/src/main/java/ru/vesuvian/service/amazons3/AmazonS3Manager.java
@@ -0,0 +1,46 @@
+package ru.vesuvian.service.amazons3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Component
+public class AmazonS3Manager {
+    final AmazonS3 s3client;
+    @Value("${yandex-cloud.bucket-name}")
+    String bucketName;
+
+    public Resource getElementByKey(String path, String fileName) {
+        String key = path + "/" + fileName;
+        S3Object s3object = s3client.getObject(new GetObjectRequest(bucketName, key));
+        return getResourceFromS3Object(s3object);
+    }
+
+    public List<String> getListObjectsInFolder(String path) {
+        ListObjectsV2Request request = new ListObjectsV2Request()
+                .withBucketName(bucketName)
+                .withPrefix(path + "/");
+        ListObjectsV2Result result = s3client.listObjectsV2(request);
+
+        return result.getObjectSummaries().stream()
+                .map(S3ObjectSummary::getKey)
+                .collect(Collectors.toList());
+    }
+
+    private Resource getResourceFromS3Object(S3Object s3object) {
+        return new InputStreamResource(s3object.getObjectContent());
+    }
+}
--- server/image-service/src/main/java/ru/vesuvian/service/service/DesignElementsService.java
+++ server/image-service/src/main/java/ru/vesuvian/service/service/DesignElementsService.java
@@ -1,18 +1,16 @@
 package ru.vesuvian.service.service;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import ru.vesuvian.service.amazons3.AmazonS3Manager;
 
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,39 +18,30 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class DesignElementsService {
-    final AmazonS3 s3client;
-    @Value("${yandex-cloud.bucket-name}")
-    String bucketName;
     @Value("${yandex-cloud.buckets.vesuvian-images.client.design-elements}")
-    String pathDesignElementsClient;
-
-    public Resource getRandomAvatar() {
-        var random = new Random();
-        // Выбрать случайный объект из списка
-        int randomNameFile = random.nextInt(7) + 1;
-        S3Object s3Object = s3client.getObject("vesuvian-images", "customers/avatars/" + randomNameFile + ".jpg");
-        return new InputStreamResource(s3Object.getObjectContent());
-    }
+    String pathDesignElements;
+    final AmazonS3Manager amazonS3Manager;
 
     public List<String> getListDesignElementNames(String page) {
-        return listObjectsInFolder(pathDesignElementsClient + page);
+        return amazonS3Manager.getListObjectsInFolder(getFullPathDesignElements(page));
     }
 
-    public Resource getDesignElementByKey(String path, String fileName) {
-        S3Object s3object = s3client.getObject(new GetObjectRequest(bucketName, pathDesignElementsClient + path + "/"  + fileName));
-        return new InputStreamResource(s3object.getObjectContent());
+    public Resource getDesignElementByKey(String folder, String fileName) {
+        return amazonS3Manager.getElementByKey(getFullPathDesignElements(folder), fileName);
     }
 
-    private List<String> listObjectsInFolder(String folder) {
-        ListObjectsV2Request request = new ListObjectsV2Request()
-                .withBucketName("vesuvian-images")
-                .withPrefix(folder + "/");
-        ListObjectsV2Result result = s3client.listObjectsV2(request);
-
-        return result.getObjectSummaries().stream()
-                .map(S3ObjectSummary::getKey)
-                .collect(Collectors.toList());
+    public String getFullPathDesignElements(String folder) {
+        return pathDesignElements + folder;
     }
+
+
+//    public Resource getRandomAvatar() {
+//        var random = new Random();
+//        // Выбрать случайный объект из списка
+//        int randomNameFile = random.nextInt(7) + 1;
+//        S3Object s3Object = s3client.getObject("vesuvian-images", "customers/avatars/" + randomNameFile + ".jpg");
+//        return new InputStreamResource(s3Object.getObjectContent());
+//    }
 
 
 }
