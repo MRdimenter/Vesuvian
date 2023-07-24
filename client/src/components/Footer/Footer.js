35481dcf97578cc27f2a1a857c20d0706c245927
KanSars <kansar13@yandex.ru>
2023-07-24 17:26:29 +0400

    style: aligned footer elements

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,16 +1,17 @@
 import { Button } from '../Button/Button';
+
 import './footer.scss';
 
 export const Footer = () => {
   return (
     <div className='footer small-text'>
-      <nav className='footer-nav'>
-        <Button btnStyle='link' label='О нас' link={'/registrationForm'} />
-        <Button btnStyle='link' label='Контакты' link={'/registrationForm'} />
-        <Button btnStyle='link' label='Блог' link={'/registrationForm'} />
-        <Button btnStyle='link' label='Что-то ещё' link={'/registrationForm'} />
-        <Button btnStyle='link' label='Ещё что-то' link={'/registrationForm'} />
-      </nav>
+      <ul className='footer-nav'>
+        <li><Button btnStyle='link' label='О нас' link={'/registrationForm'} /></li>
+        <li><Button btnStyle='link' label='Контакты' link={'/registrationForm'} /></li>
+        <li><Button btnStyle='link' label='Блог' link={'/registrationForm'} /></li>
+        <li><Button btnStyle='link' label='Что-то ещё' link={'/registrationForm'} /></li>
+        <li><Button btnStyle='link' label='Ещё что-то' link={'/registrationForm'} /></li>
+      </ul>
         <div className='copyright'>
           <p>© Vesuvian, Inc.</p>
           <p>2023</p>
