3b0d932c26da280efa78af8527fde10e828c1f22
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-12-17 21:37:10 +0400

    feat: added documentation for tag controller

--- server/collection-manager-service/src/main/java/ru/vesuvian/collection/controller/TagController.java
+++ server/collection-manager-service/src/main/java/ru/vesuvian/collection/controller/TagController.java
@@ -3,11 +3,14 @@ package ru.vesuvian.collection.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.vesuvian.collection.dto.create.TagCreateDto;
 import ru.vesuvian.collection.dto.get.TagGetDto;
@@ -16,7 +19,7 @@ import ru.vesuvian.collection.service.tag.TagService;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/{collectionId}/tags")
+@RequestMapping("api/v1/collections/{collectionId}/tags")
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Tag", description = "Tag management API")
@@ -30,7 +33,8 @@ public class TagController {
             @ApiResponse(responseCode = "200", description = "Tag successfully created"),
             @ApiResponse(responseCode = "400", description = "Invalid input"),
             @ApiResponse(responseCode = "404", description = "Collection not found"),
-            @ApiResponse(responseCode = "500", description = "Server error")
+            @ApiResponse(responseCode = "403", description = "Forbidden action for the user"),
+            @ApiResponse(responseCode = "422", description = "Collection tag limit exceeded"),
     })
     public void createTagByCollectionId(
             @PathVariable
@@ -52,7 +56,13 @@ public class TagController {
         tagService.createTagByCollectionId(collectionId, tag);
     }
 
-
+    @Operation(summary = "Get tags by collection ID",
+            description = "Retrieve a specific collection based on its ID",
+            responses = {
+                    @ApiResponse(responseCode = "404", description = "Collection not found", content = @Content(schema = @Schema(implementation = Void.class))),
+                    @ApiResponse(responseCode = "200", description = "Tag received successfully"),
+                    @ApiResponse(responseCode = "401", description = "Full authentication is required to access this resource", content = @Content(schema = @Schema(implementation = Void.class)))
+            })
     @GetMapping
     public List<TagGetDto> getTagByCollectionId(
             @PathVariable
@@ -66,6 +76,15 @@ public class TagController {
     }
 
     @DeleteMapping("/{tagId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete tag by ID",
+            description = "Removing a collection tag by collection ID and tag ID",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Tag deleted successfully"),
+                    @ApiResponse(responseCode = "404", description = "Collection or tag not found"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden action for the user"),
+                    @ApiResponse(responseCode = "401", description = "Full authentication is required to access this resource")
+            })
     public void deleteTagByCollectionIdAndTagId(
             @PathVariable
             @Parameter(description = "ID of the collection where tag will be delete",
